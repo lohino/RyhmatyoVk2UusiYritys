df0733ea979c7fe91e512d6a0d9ed203256c382f
Marko S <msalmikangas@gmail.com>
2021-07-02 16:25:50 +0300

    Varmasti toimiva systeemi

--- UI/Program.cs
+++ UI/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
 using Käsittelylogiikka;
 
 namespace UI
@@ -7,12 +9,70 @@ namespace UI
     {
         static void Main(string[] args)
         {
-            string loki;
+            string loki = "c:\\temp\\loki.txt";
+            if (!Directory.Exists("C:\\temp\\")) { Directory.CreateDirectory("c:\\temp\\"); }
             var tk = TiedostonKäsittely.LueTiedosto("");
+            int sar = tk[0].Count;
             foreach (var item in tk)
             {
+                try
+                {
 
+                    if (TiedostonKäsittely.OnOikeaMääräSarakkeita(item[6], sar))
+                    {
+                        Console.WriteLine();
+                    }
+                    else
+                    {
+                        KirjoitaLokiin(loki, "Kommentti liian pitkä");
+                    }
+                }
+                catch
+                {
+
+                }
+                try
+                {
+                    DateTime alkup = new DateTime(Convert.ToInt32(item[2]));
+                    DateTime loppu = new DateTime(Convert.ToInt32(item[3]));
+                    if (TiedostonKäsittely.Alkupaiva(alkup, loppu))
+                    {
+                        Console.WriteLine();
+                    }
+                    else
+                    {
+                        KirjoitaLokiin(loki, "virhe alkupäivämäärässä tai lopetus ennen aloitusta");
+                    }
+                }
+                catch
+                {
+                    
+                }
+
+                try
+                {
+                    
+                    if (TiedostonKäsittely.MaksimiPituus(item[6]))
+                    {
+                        Console.WriteLine();
+                    }
+                    else
+                    {
+                        KirjoitaLokiin(loki, "Kommentti liian pitkä");
+                    }
+                }
+                catch
+                {
+
+                }
+
+                
             }
+        }
+        static public void KirjoitaLokiin(string loki, string v)
+        {
+            if (!File.Exists(loki)) { File.Create(loki); }
+            File.AppendAllText(loki, v);
         }
     }
 }
